69d900d0b00861623fc9de1dd39433979281276a
Mateus Carniatto <mcarniatto@lenovo.com>
2015-03-05 15:26:49 -0300

    Changed the homescreen to the list-Punches

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -4,13 +4,13 @@
 angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 	function($stateProvider, $urlRouterProvider) {
 		// Redirect to home view when route not found
-		$urlRouterProvider.otherwise('/');
+		$urlRouterProvider.otherwise('/punches');
 
 		// Home state routing
 		$stateProvider.
 		state('home', {
-			url: '/',
-			templateUrl: 'modules/core/views/home.client.view.html'
+			url: '/punches',
+			templateUrl: 'modules/punches/views/list-punches.client.view.html'
 		});
 	}
 ]);
